4878a02540fb199dc87db54b5273ae0b6508876b
Uselessness <andrew13131336@gmail.com>
2017-01-24 17:09:30 +0400

    PISOS

--- src/Main.java
+++ src/Main.java
@@ -11,11 +11,16 @@ public class Main {
 
         System.out.println("Enter number of values in array");
         int n = scanner.nextInt();
+        long erstrt = System.nanoTime();
         Eratosphen eratosphen = new Eratosphen(n);
+        long erend = System.nanoTime();
+        System.out.println("ERATOSPHEN time = "+(erend-erstrt));
         ArrayList<Integer> a = new ArrayList<Integer>();
         String los = "";
+        int BOUND = n*n;
+
         for (int i = 0; i < n; i++) {
-            a.add(random.nextInt(n*n));
+            a.add(random.nextInt(BOUND));
             los += a.get(i)+ " ";
         }
         System.out.println(los);
@@ -24,8 +29,8 @@ public class Main {
 
         Search search = new Search();
 
-        Sort sort = new Sort();
-        sort.insertion(a);
+        //Sort sort = new Sort();
+        Sort.insertion(a);
         los = "";
         for (int i = 0; i < n; i++) {
             los += a.get(i)+ " ";
--- src/Sort.java
+++ src/Sort.java
@@ -3,27 +3,60 @@ import java.util.ArrayList;
 /**
  * Created by Dns on 24.01.2017.
  */
-public class Sort {
+public abstract class Sort {
     Sort(){
     }
 
-    public ArrayList<Integer> insertion(ArrayList<Integer> array){
+    public static ArrayList<Integer> insertion(ArrayList<Integer> array){
         for (int i = 0; i < array.size(); i++)
         {
-            int temp = array.get(i);// запомним i-ый элемент
-            int j =i-1;//будем идти начиная с i-1 элемента
+            int temp = array.get(i);
+            int j =i-1;
             while(j >= 0 && array.get(j) > temp)
-            // пока не достигли начала массива
-            // или не нашли элемент больше i-1-го
-            // который храниться в переменной temp
             {
                 array.set(j + 1, array.get(j));
-                //проталкиваем элемент вверх
                 j--;
             }
             array.set(j + 1, temp);
-            // возвращаем i-1 элемент
         }
         return array;
     }
+
+    public static ArrayList<Integer> choose(ArrayList<Integer> array){
+        for (int i = 0; i < array.size(); i++) {
+            int minValueIndex = i;
+            for (int j = i + 1; j < array.size(); j++) {
+                if (array.get(j) < array.get(minValueIndex)) {
+                    minValueIndex = j;
+                }
+            }
+            int temp = array.get(i);
+            array.set(i, array.get(minValueIndex));
+            array.set(minValueIndex, temp);
+        }
+        return array;
+    }
+
+    public static ArrayList<Integer> shell(ArrayList<Integer> array){
+        int size = array.size();
+        int step = size / 2;
+        while (step > 0)
+        {
+            for (int i = 0; i < (size - step); i++)
+            {
+                int j = i;
+
+                while (j >= 0 && array.get(j) > array.get(j + step))
+                {
+                    int temp = array.get(j);
+                    array.set(j, array.get(j + step));
+                    array.set(j + step, temp);
+                    j--;
+                }
+            }
+            step = step / 2;
+        }
+        return array;
+    }
+
 }
